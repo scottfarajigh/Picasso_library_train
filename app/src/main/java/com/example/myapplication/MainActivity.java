81d4b470f2bbbd45386e78eb02b4846c5749458e
scottfarajigh <habibfaraji2002@gmail.com>
2023-04-10 12:40:02 -0700

    library is worked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,15 +15,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView =findViewById(R.id.iv_main);
+        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMsiTwIo6vwc9046_WC4K0_e4LlTOjDDmiAA&usqp=CAU")
+                .placeholder(R.drawable.batman)
+                .error(R.drawable.c)
+                .into(imageView);
 
     }
 }
